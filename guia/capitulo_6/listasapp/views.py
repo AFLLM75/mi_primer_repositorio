88cfd79d886468361f37b219efbd9f84d1942ee7
Alberto <afllm75@gmail.com>
2020-09-21 19:26:05 +0200

    Implementar persistencia de datos de las notas

--- guia/capitulo_6/listasapp/views.py
+++ guia/capitulo_6/listasapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import psycopg2
+
 
 # Create your views here.
 def form(request):
    return render(request,'formulario.html')
-
+"""
 def anadir(request):
    prioridad = request.POST["name_prioridad"]
    titulo= request.POST["nombre_titulo"]
@@ -13,3 +15,38 @@ def anadir(request):
                         f'Prioridad: {prioridad}<br>'
                         f'Título: {titulo}<br>'
                         f'Nota: {nota}<br>'))
+"""
+
+def insert(request):
+   conn = psycopg2.connect(dbname="capitulo_6_db",
+                           user="capitulo_6_user",
+                           password="patata")
+
+   cursor = conn.cursor()
+   cursor.execute("INSERT INTO Nota VALUES ('Máxima','Cumpleaños', \
+                  'Comprar Regalo');")
+   conn.commit()
+   cursor.close()
+   conn.close()
+   return HttpResponse("Insertado")
+
+
+def select(request):
+   conn = psycopg2.connect(dbname="capitulo_6_db",
+                           user="capitulo_6_user",
+                           password="patata")
+   cursor = conn.cursor()
+   cursor.execute("select * from Nota")
+   html = '<html>'
+   columns = [col[0] for col in cursor.description]
+   for column in columns:
+       html += str(column) + '|'
+   html += '<br>'
+   for empleado in cursor.fetchall():
+       for columna in empleado:
+           html += str(columna) + '|'
+       html += '<br>'
+   html += '</html>'
+   cursor.close()
+   conn.close()
+   return HttpResponse(html)
