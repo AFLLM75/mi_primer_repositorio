e4a785c28b9a0e3edabbfaf2898733460c49ab92
Alberto <afllm75@gmail.com>
2020-09-21 17:03:21 +0200

    Añadiendo fstrings

--- guia/capitulo_6/listasapp/views.py
+++ guia/capitulo_6/listasapp/views.py
@@ -6,4 +6,10 @@ def form(request):
    return render(request,'formulario.html')
 
 def anadir(request):
-   return HttpResponse('Insertado')
+   prioridad = request.POST["name_prioridad"]
+   titulo= request.POST["nombre_titulo"]
+   nota = request.POST["name_nota"]
+   return (HttpResponse(f'Insertado <br> '
+                        f'Prioridad: {prioridad}<br>'
+                        f'Título: {titulo}<br>'
+                        f'Nota: {nota}<br>'))
